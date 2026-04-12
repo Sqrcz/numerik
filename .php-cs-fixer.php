69c8a107cfba5be7f535df53a833a93279a96171
Sqrcz <naorniakowski@slashlab.pl>
2026-04-13 00:44:12 +0200

    chore: clean up tooling configuration and add enum labels

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -8,30 +8,24 @@ $finder = PhpCsFixer\Finder::create()
         __DIR__ . '/tests',
     ])
     ->name('*.php')
-    ->notName('*.blade.php')
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
 return (new PhpCsFixer\Config())
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
+    ->setUsingCache(true)
     ->setRules([
-        '@PSR12'                         => true,
-        '@PHP82Migration'                => true,
-        'array_syntax'                   => ['syntax' => 'short'],
-        'ordered_imports'                => ['sort_algorithm' => 'alpha'],
-        'no_unused_imports'              => true,
+        '@PSR12'                            => true,
+        '@PHP82Migration'                   => true,
+        'ordered_imports'                   => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports'                 => true,
         'not_operator_with_successor_space' => true,
-        'trailing_comma_in_multiline'    => true,
-        'phpdoc_scalar'                  => true,
-        'unary_operator_spaces'          => true,
-        'binary_operator_spaces'         => true,
-        'blank_line_before_statement'    => [
+        'trailing_comma_in_multiline'       => true,
+        'blank_line_before_statement'       => [
             'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
         ],
-        'phpdoc_single_line_var_spacing' => true,
-        'phpdoc_var_without_name'        => true,
-        'declare_strict_types'           => true,
-        'no_extra_blank_lines'           => true,
-        'single_quote'                   => true,
+        'declare_strict_types'              => true,
+        'no_extra_blank_lines'              => true,
     ])
     ->setFinder($finder)
     ->setRiskyAllowed(true);
